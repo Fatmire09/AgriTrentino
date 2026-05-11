b40078cac792474e50cec1f8d7744f57ab213850
Fatmire09 <151674702+Fatmire09@users.noreply.github.com>
2026-05-11 11:31:39 +0200

    feat(US2): aggiorna server/index.js con connessione MongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-equire('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
